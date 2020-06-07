5c2668bc0e5f779ebead4e411cd52a03ce03bdd5
unknown <michaelarayamurcia@gmail.com>
2020-06-07 16:45:55 -0600

    Descripción y reglas del juego

--- Proyecto0.py
+++ Proyecto0.py
@@ -6,3 +6,7 @@ print("   %%    %%%%%     %%    %%  %%    %%    %%%%%%")
 print("   %%    %%  %%    %%     %%%%     %%    %%  %%")
 print("   %%    %%  %%  %%%%%%    %%    %%%%%%  %%  %%")
 print("        										  ")
+
+print("Descripción: Trivia es un juego en donde jugadores se reunen para ver quien sabe mas sobre un tema en especifico.")
+print("Reglas: ")
+print("1) Cada jugador debe responder 5 preguntas, durante 3 rondas \n   al final del juego el que haya acertado mas preguntas es el ganador.")
