aa4fe48224290d915214c0560222353760f0b4fb
unknown <michaelarayamurcia@gmail.com>
2020-06-07 20:02:02 -0600

    Poniendo función para mostrar puntuaciones

--- Proyecto0.py
+++ Proyecto0.py
@@ -17,7 +17,7 @@ def mensajeBienvenida():
 	print("Reglas: ")
 	print("1) Cada jugador debe responder 5 preguntas, durante 3 rondas \n   al final del juego el que haya acertado mas preguntas es el ganador.")
 
-def cantidadjugadores():
+def cantidadJugadores():
 	"""
 	Funcion que le pide al usuario la cantidad de jugadores que van a jugar y sus respectivos nombres.
 	Entrada: Ninguna.
@@ -26,7 +26,7 @@ def cantidadjugadores():
 	"""
 	aceptar = False
 
-	listadejugadores = []
+	listadejugadores = {}
 
 	while not aceptar:
 		cantidad = input("Ingrese cantidad de jugadores: ")
@@ -40,8 +40,23 @@ def cantidadjugadores():
 
 	cantidad = int(cantidad)
 
-	for jugador in range(cantidad):
-		listadejugadores.append(input("Ingrese el nombre del jugador "+ str(jugador + 1) + ": "))
+	for i in range(0,cantidad):
+		nombre = input("Ingrese el nombre del jugador " + str(i + 1) + ": ")
+		listadejugadores["jugador"+str(i+1)] = {}
+		listadejugadores["jugador"+str(i+1)]["nombre"] = nombre 
+		listadejugadores["jugador"+str(i+1)]["puntuacion"] = 0
+
+	return listadejugadores
+
+def mostrarPuntuacion(listadejugadores):
+	"""
+	Función que muestra en pantalla la lista de jugadores con sus puntuaciones.
+	Entrada: Un diccionario con los jugadores.
+	Salidas: Ninguna.
+	Restricciones: La entrada tiene que ser un diccionario. 
+	"""
+	for key in (listadejugadores):
+		print(listadejugadores[key])
 
 def abrirPreguntas():
 	archivo = open("prueba.json","r")
@@ -49,5 +64,10 @@ def abrirPreguntas():
 		print(linea)
 	archivo.close()
 
-cantidadjugadores()
-abrirPreguntas()
+
+def main():
+	mensajeBienvenida()
+	lista = cantidadJugadores()
+	mostrarPuntuacion(lista)
+
+main()
