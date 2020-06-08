523fa11c285924b71d1ee031b32534bef2e67883
unknown <michaelarayamurcia@gmail.com>
2020-06-07 18:02:28 -0600

    Verificando que la cantidad de jugadores sean un numero

--- Proyecto0.py
+++ Proyecto0.py
@@ -18,29 +18,36 @@ def mensajeBienvenida():
 	print("1) Cada jugador debe responder 5 preguntas, durante 3 rondas \n   al final del juego el que haya acertado mas preguntas es el ganador.")
 
 def cantidadjugadores():
-    """
-    Funcion que le pide al usuario la cantidad de jugadores que van a jugar y sus respectivos nombres.
-    Entrada: Ninguna.
-    Salida: La lista de jugadores que van a jugar.
-    Restricciones: La cantidad de jugadores debe de ser un entero positivo.
-    """
+	"""
+	Funcion que le pide al usuario la cantidad de jugadores que van a jugar y sus respectivos nombres.
+	Entrada: Ninguna.
+	Salida: La lista de jugadores que van a jugar.
+	Restricciones: La cantidad de jugadores debe de ser un entero positivo.
+	"""
+	aceptar = False
 
-    listadejugadores = []
-    cantidad = input("Ingrese cantidad de jugadores: ")
+	listadejugadores = []
 
-    while not cantidad.isnumeric() :
-    	print("La cantidad de jugadores tiene que ser un número.")
-    	cantidad = input("Ingrese cantidad de jugadores: ")
-    
-    cantidad = int(cantidad)
+	while not aceptar:
+		cantidad = input("Ingrese cantidad de jugadores: ")
 
-    for jugador in range(cantidad):
-        listadejugadores.append(input("Ingrese el nombre del jugador "+ str(jugador + 1) + ": "))
+		if not cantidad.isnumeric():
+			print("La cantidad tiene que ser un numero positivo.")
+		elif cantidad.isnumeric() and int(cantidad) <= 0:
+			print("La cantidad tiene que ser un numero positivo.")
+		else:
+			aceptar = True
+
+	cantidad = int(cantidad)
+
+	for jugador in range(cantidad):
+		listadejugadores.append(input("Ingrese el nombre del jugador "+ str(jugador + 1) + ": "))
 
 def abrirPreguntas():
-	archivo = open("prueba.txt","r")
+	archivo = open("prueba.json","r")
 	for linea in archivo.readlines():
 		print(linea)
 	archivo.close()
 
+cantidadjugadores()
 abrirPreguntas()
